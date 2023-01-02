cd6e21d5d9c236d325454ecff52960980cab9a4d
Mohamed Ghoneam <ghoneamm4@gmail.com>
2023-01-02 02:31:00 +0200

    fix scanf to work without fflush and replace fflush with setbuf

--- Unit_2_C_Programming/Lecture_3_C_Basic/Lecture_labs/Lab1/main.c
+++ Unit_2_C_Programming/Lecture_3_C_Basic/Lecture_labs/Lab1/main.c
@@ -11,18 +11,19 @@
 
 int main(){
 
-//	setbuf(stdout,NULL);
+	setbuf(stdout,NULL);
 
 	char choice;
 	float area , circumference , radius;
 
 	printf ("Enter circle radius : ");
-	fflush (stdin);	fflush (stdout);
-	scanf ("%f",&radius);
+	//	fflush (stdin);	fflush (stdout);
+	scanf (" %f",&radius);
 
 	printf ("enter your choice(a for circle area or c for circumference) : ");
-	fflush (stdin);	fflush (stdout);
-	scanf ("%c", &choice);
+	//	fflush (stdin);	fflush (stdout);
+	scanf (" %c", &choice); /* scanf gives new line automatically with char
+		To avoid this error, Space before the specifier %c -> scanf(" %c",&choice)*/
 	//choice=getche();
 
 	if (choice=='a'){
