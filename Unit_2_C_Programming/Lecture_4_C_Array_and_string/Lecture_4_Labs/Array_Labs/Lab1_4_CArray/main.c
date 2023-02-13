773e08f53ce3bcc62c599cb62b2a145452d97764
Mohamed Ghoneam <ghoneamm4@gmail.com>
2023-02-13 23:49:45 +0200

    solve Lab1 in lecture 4 (Array)

--- Unit_2_C_Programming/Lecture_4_C_Array_and_string/Lecture_4_Labs/Array_Labs/Lab1_4_CArray/main.c
+++ Unit_2_C_Programming/Lecture_4_C_Array_and_string/Lecture_4_Labs/Array_Labs/Lab1_4_CArray/main.c
@@ -10,7 +10,18 @@
 #include <stdio.h>
 
 int main(){
+	setbuf(stdout,NULL);
 
+	float S[10];
+	for (int i=0;i<10;i++){
+		printf("Enter student %d degree : ",i+1);
+		scanf("%f",&S[i]);
+		printf("\n");
+	}
+
+	for (int i=0;i<10;i++){
+		printf("Student %d degree is %.2f \n",i+1,S[i]);
+	}
 
 	return 0;
 }
--- Unit_2_C_Programming/Lecture_4_C_Array_and_string/Lecture_4_Labs/Array_Labs/README.md
+++ Unit_2_C_Programming/Lecture_4_C_Array_and_string/Lecture_4_Labs/Array_Labs/README.md
@@ -1,3 +1,7 @@
 # test and training with lecture4 labs (C_Array) 
 
-## Lab 1 in slide page ##
+## Lab 1 in slide page 7
+
+Store and Print 10 Students Degrees
+
+<img src="Lab1_4_CArray/unit_2_lecture_4_CArray_lab1_p7.jpg" width="600"/>
