b748977531036f495ff3c9e221954ccc8501b7b5
Mohamed Ghoneam <ghoneamm4@gmail.com>
2023-02-16 23:15:52 +0200

    solve Ex1 in assignment 3 lecture 4 array

--- Unit_2_C_Programming/Lecture_4_C_Array_and_string/C_Array_and_string_Assignment/Assignment_3/Array_HW/Ex1_3_4_CArray/main.c
+++ Unit_2_C_Programming/Lecture_4_C_Array_and_string/C_Array_and_string_Assignment/Assignment_3/Array_HW/Ex1_3_4_CArray/main.c
@@ -8,11 +8,39 @@
 
 
 #include <stdio.h>
+//Dimension of 2D array need adding
+#define k 2
+#define n 2
 
 int main(){
 	setbuf(stdout,NULL);
 
+	float a[k][n],b[k][n];
 
+	printf("Enter the elements of 1st matrix\r\n");
+	for(int i=0;i<k;i++){
+		for(int j=0;j<n;j++){
+			printf("Enter a%d%d : ",i+1,j+1);
+			scanf("%f",&a[i][j]);
+		}
+	}
+
+	printf("Enter the elements of 2st matrix\r\n");
+	for(int i=0;i<k;i++){
+		for(int j=0;j<n;j++){
+			printf("Enter b%d%d : ",i+1,j+1);
+			scanf("%f",&b[i][j]);
+		}
+	}
+
+	printf("Sum Of Matrix\n");
+	for(int i=0;i<k;i++){
+		for(int j=0;j<n;j++){
+			if(j<n-1) printf("%.1f   ",a[i][j]+b[i][j]);
+			else printf("%.1f",a[i][j]+b[i][j]);
+		}
+		printf("\n");
+	}
 
 	return 0;
 }
--- Unit_2_C_Programming/Lecture_4_C_Array_and_string/C_Array_and_string_Assignment/Assignment_3/Array_HW/README.md
+++ Unit_2_C_Programming/Lecture_4_C_Array_and_string/C_Array_and_string_Assignment/Assignment_3/Array_HW/README.md
@@ -1,3 +1,43 @@
 # Here is the Assignment(3) Array_HW for lecture 4 it has 5 exercises
 
-## 1.Solving EX1:
+## 1.Solving EX1:Example of Multidimensional Array In C
+
+Write a C program to find sum of two matrix of order 2* 2 using multidimensional arrays where, elements of matrix are entered by user.
+
+### Ouput
+
+
+	Enter the elements of 1st matrix
+	
+	Enter all: 2;
+	Enter a12: 0.5;
+	Enter a21: -1.1;
+	Enter a22: 2;
+	
+	Enter the elements of 2nd matrix
+	
+	Enter bll: 0.2;
+	Enter b12: 0;
+	Enter b21: 0.23;
+	Enter b22: 23;
+	
+	Sum Of Matrix:
+	2.2    0.5
+	-0.9   25.0
+	
+solved 
+------------------------------------------------------------
+
+
+
+
+
+
+
+
+
+
+
+
+
+
