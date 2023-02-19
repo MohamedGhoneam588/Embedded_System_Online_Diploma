b25c14053afca76589ffbfba3bfc49a7e1449f33
Mohamed Ghoneam <ghoneamm4@gmail.com>
2023-02-19 18:09:47 +0200

    solve Ex 4 Assignment 3 Lecture 4 Array

--- Unit_2_C_Programming/Lecture_4_C_Array_and_string/C_Array_and_string_Assignment/Assignment_3/Array_HW/Ex4_3_4_CArray/main.c
+++ Unit_2_C_Programming/Lecture_4_C_Array_and_string/C_Array_and_string_Assignment/Assignment_3/Array_HW/Ex4_3_4_CArray/main.c
@@ -12,7 +12,25 @@
 int main(){
 	setbuf(stdout,NULL);
 
-
+	int n,k;
+	printf("Enter no of elements (n) : ");
+	scanf("%d",&n);
+	float a_n[n],a_n1[n+1],a;
+	for(int i=0;i<n;i++)scanf("%f",&a_n[i]);
+	printf("Enter the element to be inserted : ");
+	scanf("%f",&a);
+	reenter_k:
+	printf("Enter the location : ");
+	scanf("%d",&k);
+	if(k>n+1){
+		printf("location must be equal to or less than (n+1)\n"
+				"please Reenter location again\r\n");
+		goto reenter_k;
+	}
+	for(int i=0;i<n+1;i++){
+		a_n1[i]=(i<k-1)*a_n[i]+(i==k-1)*a+(i>k-1)*a_n[i-1];
+		printf("%.0f ",a_n1[i]);
+	}
 
 	return 0;
 }
--- Unit_2_C_Programming/Lecture_4_C_Array_and_string/C_Array_and_string_Assignment/Assignment_3/Array_HW/README.md
+++ Unit_2_C_Programming/Lecture_4_C_Array_and_string/C_Array_and_string_Assignment/Assignment_3/Array_HW/README.md
@@ -77,7 +77,21 @@ solved
 ------------------------------------------------------------
 
 
+## 4.Solving EX4: C Program to Insert an element in an Array
 
+
+### Ouput
+
+
+	Enter no of elements : 5
+	1  2  3  4  5
+	Enter the element to be inserted : 6
+	Enter the location : 2
+	1  6  2  3  4  5
+
+	
+solved 
+------------------------------------------------------------
 
 
 
