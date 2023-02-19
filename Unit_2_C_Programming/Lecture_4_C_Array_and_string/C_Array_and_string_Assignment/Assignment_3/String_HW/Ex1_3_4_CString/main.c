0397cb6170b47ce0f8b4aa12c383586c2a472062
Mohamed Ghoneam <ghoneamm4@gmail.com>
2023-02-19 22:31:18 +0200

    solve Ex 1 Assignment 3 Lecture 4 String

--- Unit_2_C_Programming/Lecture_4_C_Array_and_string/C_Array_and_string_Assignment/Assignment_3/String_HW/Ex1_3_4_CString/main.c
+++ Unit_2_C_Programming/Lecture_4_C_Array_and_string/C_Array_and_string_Assignment/Assignment_3/String_HW/Ex1_3_4_CString/main.c
@@ -8,11 +8,37 @@
 
 
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
 int main(){
 	setbuf(stdout,NULL);
 
+	int n;
+	printf("Enter number of char in your string not exact but by sense"
+			" (more than your string char is better) : ");
+	scanf("%d",&n);
 
+	char string_arr[n] ,ch;
+	char K='a'-'A';
+	int i,l=0,n2;
+
+	printf("Enter a string : ");
+	fflush(stdout); fflush(stdin);
+	gets(string_arr);
+	n2=strlen(string_arr);
+
+	printf("Enter a character to find frequency: ");
+	scanf(" %c",&ch);
+
+
+	for (i=0 ;i<n2 ; i++){
+		if(string_arr[i]==ch||
+				((string_arr[i]==ch+K||string_arr[i]==ch-K)&&string_arr[i]>='A')){
+			l++;
+		}
+	}
+	printf("Frequency of %c = %d",ch,l);
 
 	return 0;
 }
--- Unit_2_C_Programming/Lecture_4_C_Array_and_string/C_Array_and_string_Assignment/Assignment_3/String_HW/README.md
+++ Unit_2_C_Programming/Lecture_4_C_Array_and_string/C_Array_and_string_Assignment/Assignment_3/String_HW/README.md
@@ -1,3 +1,15 @@
 # Here is the Assignment(3) String_HW for lecture 4 it has 3 exercises
 
-## 1.Solving EX1:
+## 1.Solving EX1: C Program to Find the Frequency of Characters in a String
+
+This program asks user to entera string and a character and this program checks how many ti mes that character is repeated in the string entered by user.
+
+### Ouput
+
+	Enter a string: This website is awesome.
+	Enter a character to find frequency: e
+	Frequency of e = 4
+
+	
+solved 
+------------------------------------------------------------
