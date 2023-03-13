96b9c3cd2393db74e31b5c3def50f53f4af1ed53
Mohamed Ghoneam <ghoneamm4@gmail.com>
2023-03-13 23:06:33 +0200

    solve lab 2 lecture 5 C functions

--- Unit_2_C_Programming/Lecture_5_C_Functions/Lecture_5_labs/Lab2_5_Cfunction/main.c
+++ Unit_2_C_Programming/Lecture_5_C_Functions/Lecture_5_labs/Lab2_5_Cfunction/main.c
@@ -7,15 +7,26 @@
  */
 
 
-
-
 #include <stdio.h>
+
+int calcMin(int values[], int n){
+	int min=values[0];
+	for (int i=1 ;i<n ;i++){
+		min= (values[i]<min) ? values[i]:min;
+	}
+	return min;
+}
 
 int main()
 {
 	setbuf(stdout,NULL);
 
+	int xvalues [10] = {35,67 ,27, 54, 76,44, 59, 32,43,25};
+	int yvalues [5] = {28,71,67,83,62};
+	int zvalues [13] = {87, 21, 74, 36, 27, 64, 87, 63, 27, 86, 48, 32, 76};
 
+	printf("The minimum of x, y, z values is (%d,%d,%d) \r\n" ,
+			calcMin (xvalues , 10), calcMin (yvalues, 5) , calcMin (zvalues,13));
 
 	return 0;
 }
--- Unit_2_C_Programming/Lecture_5_C_Functions/Lecture_5_labs/README.md
+++ Unit_2_C_Programming/Lecture_5_C_Functions/Lecture_5_labs/README.md
@@ -6,3 +6,12 @@
 Write a program uses a function to calculate the factorial of any positive number 
 
 <img src="Lab1_5_Cfunction/unit_2_lecture_5_CFunction_lab1_p11.jpg" width="600"/>
+
+## Lab 2 in slide page 13
+
+Calculatethe Minimum Value of any Given Array 
+
+	int calcMin(int values[], int n) ;
+	
+
+
