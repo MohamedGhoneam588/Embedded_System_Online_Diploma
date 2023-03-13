4c75b363c492442f0bcaab71c6a4ce2e3d7a6e0c
Mohamed Ghoneam <ghoneamm4@gmail.com>
2023-03-13 18:25:22 +0200

    solve lab 1 lecture 5 C functions

--- Unit_2_C_Programming/Lecture_5_C_Functions/Lecture_5_labs/Lab1_5_Cfunction/main.c
+++ Unit_2_C_Programming/Lecture_5_C_Functions/Lecture_5_labs/Lab1_5_Cfunction/main.c
@@ -11,10 +11,24 @@
 
 #include <stdio.h>
 
+int factorial (int x){
+	int fact=1,i;
+	for(i=1;i<=x;i++)
+		fact*=i;
+	return fact;
+}
+
 int main()
 {
 	setbuf(stdout,NULL);
-
+//	int fact,x;
+//	printf("Enter a number to calc factorial : ");
+//	scanf("%d",&x);
+//	fact=factorial(x);
+//	printf("Factorial(%d) = %d",x,fact);
+	printf("Factorial(%d) = %d\r\n",10,factorial(10));
+	printf("Factorial(%d) = %d\r\n",0,factorial(0));
+	printf("Factorial(%d) = %d\r\n",5,factorial(5));
 
 
 	return 0;
--- Unit_2_C_Programming/Lecture_5_C_Functions/Lecture_5_labs/README.md
+++ Unit_2_C_Programming/Lecture_5_C_Functions/Lecture_5_labs/README.md
@@ -1,4 +1,8 @@
 # Test and training with lecture 5 labs (C_Fenctions) 
 
 
-## Lab 1 in slide page ## lecture 5 pdf 
+## Lab 1 in slide page 11
+
+Write a program uses a function to calculate the factorial of any positive number 
+
+<img src="Lab1_5_CFunction/unit_2_lecture_5_CFunction_lab1_p11.jpg" width="600"/>
