c49453f9888cdd9c4b97390babfbee994287aab2
Mohamed Ghoneam <ghoneamm4@gmail.com>
2023-03-15 00:36:18 +0200

    solve Ex1 Assignment 4 Lecture 5 C Function

--- Unit_2_C_Programming/Lecture_5_C_Functions/C_Functions_Assignment/Assignment_4/Ex1_4_5_Cfunction/main.c
+++ Unit_2_C_Programming/Lecture_5_C_Functions/C_Functions_Assignment/Assignment_4/Ex1_4_5_Cfunction/main.c
@@ -11,11 +11,33 @@
 
 #include <stdio.h>
 
+int is_prime(int x){
+	if(x==1) return 0;
+	else{
+		for(int i=2;i<x/2;i++){
+			if(x%i==0){return 0;break;}
+		}
+		return 1;
+	}
+}
+
 int main()
 {
 	setbuf(stdout,NULL);
 
+	int i,j,k;
 
+	printf("Enter two numbers(intervals): ");
+	scanf("%d",&i);
+	scanf("%d",&j);
+	if(i>j){k=i;i=j;j=k;}
+
+	printf("Prime numbers between %d and %d are: ",i,j);
+	for(int x=i;x<=j;x++){
+		k=is_prime(x);
+		if(k==1) printf("%d ",x);
+
+	}
 
 	return 0;
 }
--- Unit_2_C_Programming/Lecture_5_C_Functions/C_Functions_Assignment/Assignment_4/README.md
+++ Unit_2_C_Programming/Lecture_5_C_Functions/C_Functions_Assignment/Assignment_4/README.md
@@ -2,4 +2,14 @@
 
 
 ## 1.Solving EX1:
+Prime Numbers Between two Intervals by Making User-defined Function
 
+### Output
+
+	Enter two numbers(intervals): 10
+	30 
+	Prime numbers between 10 and 30 are: 11 13 17 19 23 29
+
+
+solved 
+------------------------------------------------------------
